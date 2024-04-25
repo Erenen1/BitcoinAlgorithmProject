599c5561bc0b48c77d9f4ffec6961e39bbbf8dd7
Eren ÇELİK <126926648+Erenen1@users.noreply.github.com>
2024-04-25 22:34:49 +0300

    Update db.ts

--- src/models/db.ts
+++ src/models/db.ts
@@ -1,8 +1,8 @@
 import mongoose from "mongoose"
-process.env.DATABASE_URL
+
 export const connectToDatabase = async ()=>{
     try {
-        await mongoose.connect("mongodb://localhost:27017/BitcoinAlgorithm");
+        await mongoose.connect(process.env.DATABASE_URL);
         
     } catch (error) {
         console.log(error);
